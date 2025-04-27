95a9bf30ffd05c03ab117c7c40db8e1cf1e11fd8
Errett <errett.bentz@gmail.com>
2025-04-27 18:30:50 -0500

    added constants and basic game loop that draws a blank black screen.

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
-import pygame
+import pygame as p
+import constants as C
 
 def main():
+	
+	p.init()
+	
 	print("Starting Asteroids!")
+	print(f"Screen width: {C.SCREEN_WIDTH}")
+	print(f"Screen height: {C.SCREEN_HEIGHT}")
+	
+	screen = p.display.set_mode( (C.SCREEN_WIDTH, C.SCREEN_HEIGHT) )
+	
+	while True:
+
+		for event in p.event.get():
+			if event.type == p.QUIT:
+				return
+			
+		screen.fill("black")
+		p.display.flip()
+
 
 if __name__ == "__main__":
 	main()
